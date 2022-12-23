c8c2e5d5763fde41a336de1fd36c2ca5f6e79760
Hemanshu Sharma <Hemanshu00001@gmail.com>
2022-12-23 12:18:35 +0530

    Add files via upload

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': 'ced8652262mshc9b19d78f41798dp1074fbjsn19fef5bc90ec',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+};
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
